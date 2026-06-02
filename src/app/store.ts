b71c9bd6010f240ffe537aa522b1c4a116e05361
Sarang Pohare <sarang.pohare@newvision-software.com>
2026-06-02 17:50:06 +0530

    Skills Module

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import projectReducer from '../features/projects/store/projectSlice';
+import skillReducer from '../features/skills/skillSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     projects: projectReducer,
+    skills: skillReducer,
   },
 });
 
